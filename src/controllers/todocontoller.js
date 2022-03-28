940e38d96b9ef7ae8cf5a970bcc82e08e67ed984
Suraj <rauniyar.suraj731@gmail.com>
2022-03-28 21:36:15 +0530

    Update todocontoller.js

--- src/controllers/todocontoller.js
+++ src/controllers/todocontoller.js
@@ -1,5 +1,56 @@
 const express = require("express");
 
 const router = express.Router();
-const todo = require("../models/todo.model")
+const todo = require("../models/todo.model");
+const authenticate = require("../middleware/authenticate");
+
+router.get("",authenticate,async(req,res)=>{
+    try {
+        const todos= await todo.find({}).lean().exec();
+        return res.status(200).send(todos);
+    } catch (err) {
+        return  res.status(401).send({message:err.message})
+    }
+})
+
+router.post("",authenticate,async(req,res)=>{
+    req.body.userId= req.user._id
+    try {
+        const todo= await todo.create(req.body)
+        return res.status(201).send(todo);
+    } catch (err) {
+        return res.status(401).send({message:err.message})
+    }
+})
+
+router.get("/:id",authenticate,async(req,res)=>{
+    try {
+        const todo = await todo.findById(req.params.id).lean().exec();
+        return res.status(200).send(todo);
+    } catch (err) {
+        return res.status(401).send({message:err.message})
+    }
+})
+
+router.patch("/:id",authenticate,async(req,res)=>{
+    try {
+        const todo = await todo.findByIdAndUpdate(req.params.id,req.body,{new:true})
+        return res.status(200).send(todo);
+    } catch (err) {
+        return res.status(401).send({message:err.message})
+    }
+})
+
+router.delete("/:id",authenticate,async(req,res)=>{
+    try {
+        const todo = await todo.findByIdAndDelete(req.params.id)
+        return res.status(200).send(todo);
+    } catch (err) {
+        return res.status(401).send({message:err.message})
+    }
+})
+
+
+module.exports=router
+
 
